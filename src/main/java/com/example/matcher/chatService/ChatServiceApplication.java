80b0bb68ee2c728e0e839d2f43ee1fdaab11ce80
Daniil Voropaev <vorop2004@mail.ru>
2025-01-26 15:21:26 +0300

    Add enviroment handling

--- src/main/java/com/example/matcher/chatService/ChatServiceApplication.java
+++ src/main/java/com/example/matcher/chatService/ChatServiceApplication.java
@@ -18,6 +18,13 @@ public class ChatServiceApplication {
 		System.setProperty("spring.datasource.username", EnvironmentLoader.get("SPRING_DATASOURCE_USERNAME"));
 		System.setProperty("spring.datasource.password", EnvironmentLoader.get("SPRING_DATASOURCE_PASSWORD"));
 
+		
+		System.setProperty("spring.kafka.bootstrap-servers", EnvironmentLoader.get("SPRING_KAFKA_BOOTSTRAP_SERVERS", "http://localhost:9092"));
+		System.setProperty("eureka.client.service-url.defaultZone", EnvironmentLoader.get("EUREKA_DEFAULT_ZONE", "http://localhost:8761/eureka/"));
+		System.setProperty("spring.jpa.hibernate.ddl-auto", EnvironmentLoader.get("SPRING_JPA_HIBERNATE_DDL_AUTO", "update"));
+
+		System.setProperty("server.port", EnvironmentLoader.get("SERVER_PORT", "8081"));
+		System.setProperty("server.address", EnvironmentLoader.get("SERVER_ADDRESS", "0.0.0.0"));
 		SpringApplication.run(ChatServiceApplication.class, args);
 	}
 
