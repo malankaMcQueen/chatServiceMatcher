fa025b4e93ba3515f434e76f9ced8c8ff7f769d1
Daniil Voropaev <vorop2004@mail.ru>
2025-01-26 05:09:37 +0300

    Add PreSend handling and environment handling

--- src/main/java/com/example/matcher/chatService/socket/WebSocketEventListener.java
+++ src/main/java/com/example/matcher/chatService/socket/WebSocketEventListener.java
@@ -1,22 +1,19 @@
 package com.example.matcher.chatService.socket;
 
-import com.example.matcher.chatService.configuration.WebSocketConfiguration;
 import com.example.matcher.chatService.dto.StatusConnectionUpdate;
+import com.example.matcher.chatService.exception.InvalidCredentialsException;
 import com.example.matcher.chatService.kafka.KafkaProducerService;
 import com.example.matcher.chatService.model.StatusConnection;
 import lombok.AllArgsConstructor;
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.security.Principal;
 import java.util.UUID;
 
 @Component
@@ -27,32 +24,42 @@ public class WebSocketEventListener {
     private final KafkaProducerService kafkaProducerService;
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-//        String userId = headerAccessor.getUser() != null ? headerAccessor.getUser().getName() : "Unknown";
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        logger.info("USER {} connected", authentication.getName());
-        StatusConnectionUpdate statusConnectionUpdate = new StatusConnectionUpdate(UUID.fromString(authentication.getName()), StatusConnection.ONLINE);
-        kafkaProducerService.sendMessage(statusConnectionUpdate, "status-connection-update");
-        // Логика при подключении пользователя
-//        System.out.println("User connected: " + userId);
-
-        // Например, обновляем статус пользователя в Redis
-        // redisTemplate.opsForValue().set("user_status:" + userId, "online");
+//        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//        String userId = accessor.getFirstNativeHeader("X-User-Id"); // Получаем ID пользователя из заголовков
+        Principal user = StompHeaderAccessor.wrap(event.getMessage()).getUser();
+        if (user != null) {
+            logger.info("User connected: {}", user.getName());
+        }
+        else {
+            throw new InvalidCredentialsException("Not found userID in Header: X-User-Id: {uuid}");
+        }
+        String userId = user.getName();
+        if (userId != null) {
+            StatusConnectionUpdate statusConnectionUpdate = new StatusConnectionUpdate(UUID.fromString(userId), StatusConnection.ONLINE);
+            kafkaProducerService.sendMessage(statusConnectionUpdate, "status_connection_update");
+            logger.info("User connected: {}", userId);
+        } else {
+            logger.warn("Connection attempt without user ID.");
+        }
     }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-//        String userId = headerAccessor.getUser() != null ? headerAccessor.getUser().getName() : "Unknown";
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Principal user = StompHeaderAccessor.wrap(event.getMessage()).getUser();
+        if (user != null) {
+            logger.info("User connected: {}", user.getName());
+        }
+        else {
+            throw new InvalidCredentialsException("Not found userID in Header: X-User-Id: {uuid}");
+        }
+        String userId = user.getName();
 
-        logger.info("USER {} DISCONNECTED", authentication.getName());
-        StatusConnectionUpdate statusConnectionUpdate = new StatusConnectionUpdate(UUID.fromString(authentication.getName()), StatusConnection.OFFLINE);
-        kafkaProducerService.sendMessage(statusConnectionUpdate, "status_connection_update");
-        // Логика при отключении пользователя
-//        System.out.println("User disconnected: " + userId);
-
-        // Например, удаляем статус из Redis
-        // redisTemplate.delete("user_status:" + userId);
+        if (userId != null) {
+            StatusConnectionUpdate statusConnectionUpdate = new StatusConnectionUpdate(UUID.fromString(userId), StatusConnection.OFFLINE);
+            kafkaProducerService.sendMessage(statusConnectionUpdate, "status_connection_update");
+            logger.info("User disconnect: {}", userId);
+        } else {
+            logger.warn("User disconnect without user ID.");
+        }
     }
 }
